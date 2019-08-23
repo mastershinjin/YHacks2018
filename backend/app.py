e4230ce2c2273a30dddf6a6d96c6d83a38541abd
Robert Yang <chickennugget181@gmail.com>
2019-08-22 20:17:34 -0400

    Major Updates
    UPDATES:
    -Fixed backend server to work with smatrcar connect now using the authorizationURL
    -added an index page (needs basic HTML or JS first)
    -added appropriate sqlite3 code to go directly with app including connecting with database, querying, etc...
    
    FUTURE:
    -test out codes, comments show where I was unsure about some stuff
    -generate HTML templates for flask to handle pages
    -migrate to AWS E3 instance
    -add Alexa support?

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,101 @@
 import smartcar
-from flask import Flask, request, jsonify
+import sqlite3
+import random
+from flask import Flask, request, jsonify, redirect, g
 from flask_cors import CORS
 
 import sys, os
 import time
 
-import SQlitetest
-
-file_dir = os.path.dirname("/Users/robertyang/PycharmProjects/backend/SmartCar_Backend/SQlitetest.py")
-sys.path.append(file_dir)
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 CORS(app)
 
+#absolute path to .db database, will change depending on user...(config?)
+DATABASE = 'C:\Users\Robert Yang\Documents\YHacks2018-master\backend\users.db'
+
+#code to retrieve database, only executed during active app context
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row #this will convert data retrieved from db from a tuple into a dictionary essentially
+    return db
+
+#function to do queries, "query" arg is a string in SQL to execute, and
+#args is going to be whatever args you want to store results
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 client = smartcar.AuthClient(
     client_id=app.config["CLIENT_ID"],
     client_secret=app.config["CLIENT_SECRET"],
     redirect_uri=app.config["REDIRECT_URI"],
-    test_mode=True,
+    test_mode=True
 )
 
 access = None
+code = None
+user_id = None
+user_email = None
+#code based off of smartcar api docs to automatically get a refresh Token
+#obviously, you must have gone through the /exchange URI before you can run this method
+def get_fresh_access():
+    # this query should fetch ONLY ONE user from the database based on their user_id
+    access = query_db('SELECT * FROM users_info WHERE userID = ?', [user_id], one=True)
+    if smartcar.is_expired(access['expiration']):
+        new_access = client.exchange_refresh_token(access['refresh_token'])
+        #modify the database to reflect the change of tokens:
+        query_db('UPDATE users_info SET access_tokens = ? WHERE userID = ?', [new_access, user_id], one=True)
+        return new_access
+    else:
+        return access
 
-# htpp://localhost:8000/exchange?code=<authorization_code>
+#this will close connection with database
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+#we need something for the index/default page
+@app.route('/')
+def index():
+    cur = get_db().cursor()
+    #maybe some sort of HTML here?...
+
+#going to add a separate login route
+@app.route('/login', methods=['GET'])
+def login():
+    # TODO: Authorization Step 1b: Launch Smartcar authentication dialog
+    auth_url = client.get_auth_url()
+    return redirect(auth_url)
+
+# htpp://localhost:3000/exchange?code=<authorization_code>
 @app.route('/exchange', methods=['GET'])
 def exchange():
-    code = request.args.get('code')
+    # notice I made all these vars global just for simplicity sake, for a real secure application, this would not be the case...
+    global code = request.args.get('code')
+    #we need to account for when users deny permission for anything using exception handling:
     global access
     access = client.exchange_code(code)
-
+    #getting their email:
+    global user_email = request.form.get('email') #not sure if this will work since email/etc... is inputeed into smartcar connect?
+    #generate a random userID:
+    global user_id = random.randint(1,100) #if necessary, we can check to make sure the random int isn't in the database using SQL querying
+    #here we insert values into the sql database:
+    query_db('INSERT INTO users_info VALUES ?', [user_email, user_id, code, access], one=True)
     return '', 200
 
 # htpp://localhost:8000/vehicles
 @app.route('/vehicles', methods=['GET'])
 def vehicles():
     global access
+    access = get_fresh_access()
     vehicle_ids = smartcar.get_vehicle_ids(access['access_token'])['vehicles']
     cars = []
 
@@ -49,6 +109,7 @@ def vehicles():
 @app.route('/vehicle/<vehicle_id>', methods=['GET'])
 def vehicle(vehicle_id):
     global access
+    access = get_fresh_access()
     vehicle = smartcar.Vehicle(vehicle_id, access['access_token'])
     info = vehicle.info()
     print(info)
@@ -59,6 +120,7 @@ def vehicle(vehicle_id):
 @app.route('/vehicle/<vehicle_id>/unlock', methods=['GET'])
 def unlock(vehicle_id):
     global access
+    access = get_fresh_access()
     vehicle = smartcar.Vehicle(vehicle_id, access['access_token'])
     response = vehicle.unlock()
 
@@ -68,6 +130,7 @@ def unlock(vehicle_id):
 @app.route('/vehicle/<vehicle_id>/lock', methods=['GET'])
 def lock(vehicle_id):
     global access
+    access = get_fresh_access()
     vehicle = smartcar.Vehicle(vehicle_id, access['access_token'])
     response = vehicle.lock()
 
@@ -77,11 +140,21 @@ def lock(vehicle_id):
 @app.route('/vehicle/<vehicle_id>/location', methods=['GET'])
 def location(vehicle_id):
     global access
+    access = get_fresh_access()
     vehicle = smartcar.Vehicle(vehicle_id, access['access_token']);
     location = vehicle.location()
     # new_access = client.exchange_refresh_token(access['refresh_token'])
 
     return jsonify(location);
+#logout endpoint to disconnet application for a particular vehicle
+@app.route('/logout', methods=['GET'])
+def logout(vehicle_id):
+    global access
+    close_connection() #this might be valid, might not be
+    access = get_fresh_access()
+    vehicle = smartcar.vehicle(vehicle_id, access['access_token']);
+    vehicle.disconnet()
+
 
 """
 #get's car info for a user based on the facebookID
@@ -125,4 +198,4 @@ fresh_access_token = get_fresh_access()['access_token']
 """
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=3000)
